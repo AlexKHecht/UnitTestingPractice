120d3f4c18a3ff0ca81acd543607d8ebbc554315
AlexKHecht <hecht.kem@gmail.com>
2024-06-29 20:35:00 -0300

    feat: first tests(simple tests on addition with Assert.Equal() for int and doubles). Also Includes project setup bc forgot to make a commit for that, oops

---
+++ UnitTesting1.Tests/CalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using UnitTesting1;
+
+namespace UnitTesting1.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void AddTest_GivenTwoInts_ReturnInt()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void AddDoubleTest_GivenTwoDouble_ReturnsDouble()
+        {
+            var calculator = new Calculator();
+            var result = calculator.AddDouble(1.3, 3.2);
+            Assert.Equal(4.5, result);
+        }
+
+    }
+}
---
+++ UnitTesting1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting1
+{
+    public class Calculator
+    {
+        public int Add (int x, int y)
+        {
+
+            return x + y;
+            
+        }
+        public double AddDouble (double x, double y)
+        {
+            return x + y; 
+        }
+    }
+}
